3dbe66be68671191d772402cb68a8d98006d2b27
Jesse Jaggars <jjaggars@redhat.com>
2017-09-18 15:55:52 -0400

    Adding json handling for image analysis response
    Added structs to unpack json response from upload/image
    Added persist function to save a copy of analsis for later viewing

--- cmd/insights-ocp.go
+++ cmd/insights-ocp.go
@@ -1,21 +1,70 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
-	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"time"
 )
+
+var databaseDir = ".insights-ocp-data"
 
 type Request struct {
 	ContentPath string
 	ImageId     string
 }
 
-type Response struct {
-	Body []byte
+type System struct {
+	Hostname string            `json:"hostname"`
+	Metadata map[string]string `json:"metadata"`
 }
+
+type Content struct {
+	Plain string `json:"plain"`
+	Html  string `json:"html"`
+}
+
+type Report struct {
+	RuleData       map[string]interface{} `json:"rule_data"`
+	Title          *Content               `json:"title"`
+	Summary        *Content               `json:"summary"`
+	Description    *Content               `json:"description"`
+	Details        *Content               `json:"details"`
+	Reference      *Content               `json:"reference"`
+	Resolution     *Content               `json:"resolution"`
+	Severity       string                 `json:"severity"`
+	Category       string                 `json:"category"`
+	Impact         int                    `json:"impact"`
+	Likelihood     int                    `json:"likelihood"`
+	RebootRequired bool                   `json:"reboot_required"`
+	Acks           []interface{}          `json:"acks"`
+}
+
+type ClientResponse struct {
+	Version string                 `json:"version"`
+	System  *System                `json:"system"`
+	Reports map[string]Report      `json:"reports"`
+	Upload  map[string]interface{} `json:"upload"`
+}
+
+// TODO can we just depend on this code from image inspector directly?
+type ImageInspectorResponse struct {
+	Name           string                  `json:"name"`
+	ScannerVersion string                  `json:"scannerVersion"`
+	TimeStamp      time.Time               `json:"timestamp"`
+	Reference      string                  `json:"reference"`
+	Description    string                  `json:"description"`
+	Summary        []ImageInspectorSummary `json:"summary`
+}
+
+type ImageInspectorSummary struct {
+	Label Severity `json:"label"`
+}
+
+type Severity string
 
 func inspect(w http.ResponseWriter, r *http.Request) {
 	var req Request
@@ -28,7 +77,7 @@ func inspect(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	if (req.ContentPath == "" || req.ImageId == "") {
+	if req.ContentPath == "" || req.ImageId == "" {
 		http.Error(w, "Invalid request", 400)
 		return
 	}
@@ -37,37 +86,52 @@ func inspect(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(req.ImageId)
 	result, err := scanImage(req.ContentPath, req.ImageId)
 	if err != nil {
+		fmt.Println(err)
 		http.Error(w, "Unable to scan image.", 500)
 		return
 	} else {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(result.Body)
+		emit(w, result)
 	}
 }
 
-func scanImage(contentPath string, imageId string) (*Response, error) {
+func persist(imageId string, data *ClientResponse) {
+	var targ = filepath.Join(databaseDir, imageId[:2], imageId) + ".json"
+	os.MkdirAll(filepath.Dir(targ), 0700)
+	fp, _ := os.Create(targ)
+	json_text, _ := json.Marshal(&data)
+	fp.Write(json_text)
+	fp.Close()
+}
 
-	cmdStr := "insights-client --mountpoint " + contentPath
+func echo(w http.ResponseWriter, r *http.Request) {
+	var doc ClientResponse
+	err := json.NewDecoder(r.Body).Decode(&doc)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	emit(w, &doc)
+}
+
+func emit(w http.ResponseWriter, resp *ClientResponse) {
+	w.Header().Set("Content-Type", "application/json")
+	var json_text, _ = json.Marshal(resp)
+	w.Write(json_text)
+}
+
+func scanImage(contentPath string, imageId string) (*ClientResponse, error) {
+	cmdStr := "insights-client --analyze-image --verbose --to-json --mountpoint " + contentPath
 	out, _ := exec.Command("/bin/sh", "-c", cmdStr).Output()
-	fmt.Printf("%s", cmdStr + "\n")
+	fmt.Printf("%s", cmdStr+"\n")
 	fmt.Printf("%s", out)
-	body, err := loadResults()
-	if err != nil {
-		return nil, err
-	}
-	return &Response{Body: body}, nil
-}
-
-func loadResults() ([]byte, error) {
-	body, err := ioutil.ReadFile("/etc/insights-client/.last-upload.results")
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
+	var res ClientResponse
+	json.Unmarshal(out, &res)
+	persist(imageId, &res)
+	return &res, nil
 }
 
 func main() {
 	http.HandleFunc("/inspect", inspect)
-	http.ListenAndServe(":9000", nil)  //TODO don't hard code port
+	http.HandleFunc("/echo", echo)
+	http.ListenAndServe(":9000", nil) //TODO don't hard code port
 }
-
