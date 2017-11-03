46a5349785c94ced9befc92e2b36cb69d8d98492
Jeremy Crafts <jcrafts@redhat.com>
2017-11-03 14:27:37 -0400

    add content, update dockerfile, osd config

--- server.js
+++ server.js
@@ -2,19 +2,37 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const bodyParser = require('body-parser');
 const pugToHtml = require('./pugToHtml');
 
 const app = express();
 const appRoot = path.join(__dirname, 'app');
 const templateDir = path.join(appRoot, 'templates');
 const publicDir = path.join(appRoot, 'public');
+const scriptsDir = path.join(appRoot, 'scripts');
 
-app.set('port', 8081);
+// app config
+app.set('port', 8080);
 app.use(express.static(publicDir));
+app.use('/scripts', express.static(scriptsDir));
 
+// routes
+app.post('/report', bodyParser.json(), (req, res) => {
+    //
+});
+
+app.get('/report/:id', (req, res) => {
+    //
+});
+
+
+// initialize~
 pugToHtml(templateDir, publicDir);
-
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Big Zam listening on port %d...', app.get('port'));
+});
+
+process.on('SIGINT', function() {
+    process.exit();
 });
 
