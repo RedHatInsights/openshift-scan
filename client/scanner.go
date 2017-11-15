b3211e78dec360bb7cb57569db1696184b571071
Lindani Phiri <lphiri@redhat.com>
2017-11-15 16:25:45 -0500

    Yet more init stuff

--- client/scanner.go
+++ client/scanner.go
@@ -23,12 +23,13 @@ func NewDefaultScanner()(Scanner){
 	return &DefaultScanner{}
 }
 
-func (s * DefaultScanner )ScanImage(contentPath string, imageId string) (response *common.ScanResponse, rawResponse *[]byte, err error) {
+func (s * DefaultScanner )ScanImage(contentPath string, imageId string) (*common.ScanResponse, *[]byte, error) {
 	cmdStr := fmt.Sprintf(clientCmd, contentPath)
-	*rawResponse, err = exec.Command("/bin/sh", "-c", cmdStr).Output()
+	var scanResp common.ScanResponse
+	jsonResp, err := exec.Command("/bin/sh", "-c", cmdStr).Output()
     if err != nil {
-		return 
+		return nil, nil, err
 	}
-	err = json.Unmarshal(*rawResponse, &response)
-	return 
+	err = json.Unmarshal(jsonResp, &scanResp)
+	return &scanResp, &jsonResp, err
 }
